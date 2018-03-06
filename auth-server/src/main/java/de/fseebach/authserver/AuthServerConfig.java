83b18c96e407a8cc7a358823e77f50ec861b5641
Frank Seebach <frank@fseebach.de>
2018-03-06 12:34:49 +0100

    domain stuff

--- auth-server/src/main/java/de/fseebach/authserver/AuthServerConfig.java
+++ auth-server/src/main/java/de/fseebach/authserver/AuthServerConfig.java
@@ -60,7 +60,7 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter{
 					.secret("{noop}acmesecret")
 					.scopes("read", "write")
 					.authorizedGrantTypes("authorization_code", "client_credentials", "password", "implicit")
-					.redirectUris("http://localhost/");
+					.redirectUris("http://localhost/", "http://fseebach.de");
 	}
 	
 }
